401a7bd6f37143a5a37d206f4f9f413f8402cb63
Vaishnavi-Golhar <169170051+Vaishnavi-Golhar@users.noreply.github.com>
2024-09-04 12:49:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Vaishnavi');
+    res.send('Hello Vaishnavi and Jungkook');
 });
 
 const PORT = process.env.PORT || 3000;
